3035bf481db127d42ae3e9bd692eb999a83073bc
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2018-06-14 19:03:36 +0200

    Add S3 file copying.

--- src/hw11/aws-script2.sh
+++ src/hw11/aws-script2.sh
@@ -36,20 +36,41 @@ done
 # echo elapsed time 
 echo "Starting the instance took $(($SECONDS-$START_TIME)) seconds!"
 
+# create an S3 bucket
+echo "Creating S3 bucket ..."
+aws s3 mb s3://doetlingerlukas-test-bucket
+
 # inside the instance
 echo "Connecting to instance at $correct_ip ..."
 ssh -o StrictHostKeyChecking=no -i $key_location ec2-user@$correct_ip << EOF
-	sudo yum -y update
-	sudo yum -y groupinstall "Development Tools"
+	aws configure set aws_access_key_id $access_key
+    aws configure set aws_secret_access_key $secret_access_key
+	aws configure set default.region eu-west-2
 	fallocate -l 1K F1.dat
-  	fallocate -l 10K F2.dat
-  	fallocate -l 100k F3.dat
-  	fallocate -l 1M F4.dat
-  	fallocate -l 10M F5.dat
-  	ls
+	fallocate -l 10K F2.dat
+	fallocate -l 100k F3.dat
+	fallocate -l 1M F4.dat
+	fallocate -l 100M F5.dat
+	ls
+	echo "--------------------------"
+	/usr/bin/time -f "%e" aws s3 cp F1.dat s3://doetlingerlukas-test-bucket
+	echo "seconds for F1 to S3"
+	/usr/bin/time -f "%e" aws s3 cp F2.dat s3://doetlingerlukas-test-bucket
+	echo "seconds for F2 to S3"
+	/usr/bin/time -f "%e" aws s3 cp F3.dat s3://doetlingerlukas-test-bucket
+	echo "seconds for F3 to S3"
+	/usr/bin/time -f "%e" aws s3 cp F4.dat s3://doetlingerlukas-test-bucket
+	echo "seconds for F4 to S3"
+	/usr/bin/time -f "%e" aws s3 cp F5.dat s3://doetlingerlukas-test-bucket
+	echo "seconds for F5 to S3"
+	aws s3 ls s3://doetlingerlukas-test-bucket
 	echo "Done!"
 EOF
 
-# terminate the instance
+#scp -o StrictHostKeyChecking=no -i $key_location ec2-user@$correct_ip:/home/ec2-user/time.txt .
+
+# terminate the instance and delete the bucket 
 echo "Terminating instance ..."
 aws ec2 terminate-instances --instance-ids $correct_id --query 'TerminatingInstances[0].CurrentState.Name'
+echo "Deleting S3 bucket ..."
+aws s3 rb s3://doetlingerlukas-test-bucket --force
