8b77e7cfd2b41533f9ca00541f932ce6c8373ff3
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2018-06-15 01:29:43 +0200

    Correctly calculate time.

--- src/hw11/aws-vm1-script.sh
+++ src/hw11/aws-vm1-script.sh
@@ -9,6 +9,7 @@ aws configure set aws_access_key_id $access_key
 aws configure set aws_secret_access_key $secret_access_key
 aws configure set default.region eu-west-2
 
+# create local files 
 fallocate -l 1K F1.dat
 fallocate -l 10K F2.dat
 fallocate -l 100k F3.dat
@@ -16,15 +17,13 @@ fallocate -l 1M F4.dat
 fallocate -l 100M F5.dat
 ls
 echo "--------------------------"
-/usr/bin/time -f "%e" aws s3 cp F1.dat s3://doetlingerlukas-test-bucket
-echo "$(command) seconds for F1 to S3" >> time.txt
-/usr/bin/time -f "%e" aws s3 cp F2.dat s3://doetlingerlukas-test-bucket
-echo "seconds for F2 to S3"
-/usr/bin/time -f "%e" aws s3 cp F3.dat s3://doetlingerlukas-test-bucket
-echo "seconds for F3 to S3"
-/usr/bin/time -f "%e" aws s3 cp F4.dat s3://doetlingerlukas-test-bucket
-echo "seconds for F4 to S3"
-/usr/bin/time -f "%e" aws s3 cp F5.dat s3://doetlingerlukas-test-bucket
-echo "seconds for F5 to S3"
+
+# write files to S3 bucket and calculate time
+echo "Copying files to S3 ..."
+echo "$({ TIMEFORMAT=%E; time aws s3 cp F1.dat s3://doetlingerlukas-test-bucket &>logfile; } 2>&1) seconds for F1 to S3 from VM1" >> time.txt
+echo "$({ TIMEFORMAT=%E; time aws s3 cp F2.dat s3://doetlingerlukas-test-bucket &>logfile; } 2>&1) seconds for F2 to S3 from VM1" >> time.txt
+echo "$({ TIMEFORMAT=%E; time aws s3 cp F3.dat s3://doetlingerlukas-test-bucket &>logfile; } 2>&1) seconds for F3 to S3 from VM1" >> time.txt
+echo "$({ TIMEFORMAT=%E; time aws s3 cp F4.dat s3://doetlingerlukas-test-bucket &>logfile; } 2>&1) seconds for F4 to S3 from VM1" >> time.txt
+echo "$({ TIMEFORMAT=%E; time aws s3 cp F5.dat s3://doetlingerlukas-test-bucket &>logfile; } 2>&1) seconds for F5 to S3 from VM1" >> time.txt
 aws s3 ls s3://doetlingerlukas-test-bucket
 echo "Done!"
